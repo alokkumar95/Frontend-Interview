fc9c34654fea4df75c3ecf8891334c923a551d1b
alok <aloke.kumar95@gmail.com>
2023-10-11 00:06:28 +0530

    added recursive dfs

---
+++ ProblemSolving/Graph/dfs/RecursiveDFS.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// DFS function for node u and graph adj
+void dfs(vector<int> adj[],int u,vector<bool> &visited){
+    // Marked u as visited
+    visited[u] = true;
+    cout<<u<<" ";
+
+    // Travelling over all the neighboring nodes
+    for(int i=0;i<adj[u].size();i++){
+
+        // v is the adjacent node
+        int v = adj[u][i];
+
+        // if v has not been visited before
+        // then perform DFS on it again
+        if(visited[v]==false){
+            dfs(adj,v,visited);
+        }
+    }
+}
+
+int main(){
+    // Number of nodes
+    int n = 5;
+
+    // Adjacency List for a directed Graph
+    vector<int> adj[n];
+    adj[0] = {1,2,3};
+    adj[1] = {4};
+    adj[2] = {};
+    adj[3] = {2};
+    adj[4] = {};
+
+    // initialize the visited array with false;
+    vector<bool> visited(n,false);
+
+    //Let's start the DFS from node 0
+    dfs(adj,0,visited);
+
+}
